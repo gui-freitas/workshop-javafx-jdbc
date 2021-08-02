d9f99dce28ff2e5e4c04ad7febcade8ac5124430
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-02 16:09:02 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String st) {
+		try {
+			return Double.parseDouble(st);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
